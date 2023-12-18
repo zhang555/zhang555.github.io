81c26c1a2ec59ba49f685110909f1dcacb70f2fc
zhanggong <zhanggong@xinyu668.com>
2023-12-18 19:50:45 +0800

    new

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,93 +1,102 @@
-import { navbar } from "vuepress-theme-hope";
+import {navbar} from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
-  // "/demo/",
+    "/",
+    // "/demo/",
 
-  // {
-  //   text: "后端开发",
-  //   icon: "book",
-  //   link: "/backend/",
-  // },
+    // {
+    //   text: "后端开发",
+    //   icon: "book",
+    //   link: "/backend/",
+    // },
 
-  {
-    text: "后端",
-    icon: "book",
-    link: "/backend/",
-  },
+    {
+        text: "后端",
+        icon: "book",
+        link: "/backend/",
+    },
 
-  {
-    text: "AI技术",
-    icon: "book",
-    link: "/ai/",
-  },
+    {
+        text: "AI技术",
+        icon: "book",
+        link: "/ai/",
+    },
 
-  {
-    text: "AI工具",
-    icon: "book",
-    link: "/tools/",
-  },
+    {
+        text: "AI工具",
+        // icon: "book",
+        icon: "laptop-code",
 
-  {
-    text: "最新资讯",
-    icon: "book",
-    link: "/news/",
-  },
+        link: "/tools/",
+    },
+
+    {
+        text: "最新资讯",
+        // icon: "ellipsis",
+        icon: "lightbulb",
+        link: "/news/",
+    },
+
+    {
+        text: "欢迎交流",
+        icon: "book",
+        link: "/contact/",
+    },
 
 
-  {
-    text: "欢迎交流",
-    icon: "book",
-    link: "/contact/",
-  },
+    {
+        text: "旧版博客",
+        icon: "blog",
+        link: "https://www.yuque.com/aixuexidenull/oe9u1k/",
+    },
 
-  // {
-  //   text: "博文",
-  //   icon: "pen-to-square",
-  //   prefix: "/posts/",
-  //   children: [
-  //     {
-  //       text: "苹果",
-  //       icon: "pen-to-square",
-  //       prefix: "apple/",
-  //       children: [
-  //         { text: "苹果1", icon: "pen-to-square", link: "1" },
-  //         { text: "苹果2", icon: "pen-to-square", link: "2" },
-  //         "3",
-  //         "4",
-  //       ],
-  //     },
-  //     {
-  //       text: "香蕉",
-  //       icon: "pen-to-square",
-  //       prefix: "banana/",
-  //       children: [
-  //         {
-  //           text: "香蕉 1",
-  //           icon: "pen-to-square",
-  //           link: "1",
-  //         },
-  //         {
-  //           text: "香蕉 2",
-  //           icon: "pen-to-square",
-  //           link: "2",
-  //         },
-  //         "3",
-  //         "4",
-  //       ],
-  //     },
-  //     { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-  //     { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-  //     "tomato",
-  //     "strawberry",
-  //   ],
-  // },
+    // {
+    //   text: "博文",
+    //   icon: "pen-to-square",
+    //   prefix: "/posts/",
+    //   children: [
+    //     {
+    //       text: "苹果",
+    //       icon: "pen-to-square",
+    //       prefix: "apple/",
+    //       children: [
+    //         { text: "苹果1", icon: "pen-to-square", link: "1" },
+    //         { text: "苹果2", icon: "pen-to-square", link: "2" },
+    //         "3",
+    //         "4",
+    //       ],
+    //     },
+    //     {
+    //       text: "香蕉",
+    //       icon: "pen-to-square",
+    //       prefix: "banana/",
+    //       children: [
+    //         {
+    //           text: "香蕉 1",
+    //           icon: "pen-to-square",
+    //           link: "1",
+    //         },
+    //         {
+    //           text: "香蕉 2",
+    //           icon: "pen-to-square",
+    //           link: "2",
+    //         },
+    //         "3",
+    //         "4",
+    //       ],
+    //     },
+    //     { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+    //     { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+    //     "tomato",
+    //     "strawberry",
+    //   ],
+    // },
 
-  // {
-  //   text: "V2 文档",
-  //   icon: "book",
-  //   link: "https://theme-hope.vuejs.press/zh/",
-  // },
+    // {
+    //   text: "V2 文档",
+    //   icon: "book",
+    //   link: "https://theme-hope.vuejs.press/zh/",
+    // },
 
 
 ]);
