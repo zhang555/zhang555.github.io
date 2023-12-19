dda8b90bda00fe5d645ba9fedb03fd06ba18c084
zhang555 <4598181@qq.com>
2023-12-19 15:05:21 +0000

    Deploying to master from @ zhang555/zhang555.github.io@c039223c35b49c9d782e37008f9e00e4cf936fdd 🚀

---
+++ assets/MyComponent-UftcObW3.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{t as o}from"./app-ekW53bd4.js";const n={props:{text:{type:String,default:""}}};function r(a,p,t,s,_,c){return" aaa "+o(t.text)}const m=e(n,[["render",r],["__file","MyComponent.vue"]]);export{m as default};
