8e0f3ba24493bca11f3371b9358d50c4767190a3
zhang555 <4598181@qq.com>
2024-03-29 03:42:49 +0000

    Deploying to master from @ zhang555/zhang555.github.io@76c46943e965970eaa7485276b0cd9d136b2a77f 🚀

---
+++ assets/Hardhat入门.html-pNxCTNnD.js
@@ -0,0 +1,29 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,e as a}from"./app-Y7Y989CC.js";const d={},s=a(`<h2 id="hardhat入门" tabindex="-1"><a class="header-anchor" href="#hardhat入门" aria-hidden="true">#</a> Hardhat入门</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>安装
+npm install --save-dev hardhat
+
+初始化项目
+npx hardhat
+
+编译合约
+npx hardhat compile
+
+测试合约
+npx hardhat test
+
+部署合约
+npx hardhat run scripts/deploy.ts
+
+启动一个本地的以太坊
+npx hardhat node
+
+部署合约到本地的以太坊
+npx hardhat run scripts/deploy.ts --network localhost
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="hardhat官网" tabindex="-1"><a class="header-anchor" href="#hardhat官网" aria-hidden="true">#</a> Hardhat官网</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>https://hardhat.org/
+
+官方教程：
+https://hardhat.org/tutorial
+
+中文教程：
+https://learnblockchain.cn/docs/hardhat/getting-started/#%E6%A6%82%E8%BF%B0
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),r=[s];function l(t,v){return n(),i("div",null,r)}const u=e(d,[["render",l],["__file","Hardhat入门.html.vue"]]);export{u as default};
---
+++ assets/智能合约.html-mOHaA9LH.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as o,a as e,b as r}from"./app-Y7Y989CC.js";const _={},c=e("h2",{id:"智能合约",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#智能合约","aria-hidden":"true"},"#"),r(" 智能合约")],-1),s=[c];function n(d,i){return a(),o("div",null,s)}const f=t(_,[["render",n],["__file","智能合约.html.vue"]]);export{f as default};
